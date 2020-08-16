c833462c9e9a238024fd430502fd5960d2120af7
Ismael Aderdor <info@iaderdor.info>
2020-08-16 13:20:21 +0200

    Refactorize app into classes

--- calculator.js
+++ calculator.js
@@ -1,80 +1,127 @@
-const display = {
-  upper: document.querySelector('#displayline1'),
-  bottom: document.querySelector('#displayline2'),
-  addCharacter: function (symbol) {
-    if (this.allowedInDisplay()) {
-      this.upper.textContent += symbol;
-    }
-  },
-  removeCharacter: function () {
-    this.upper.textContent = display.upper.textContent.slice(0, -1);
-  },
-  clearDisplay: function () {
-    this.upper.textContent = '';
-    this.bottom.textContent = '0';
-  },
-  addBehaviourToButton: function (button, symbol) {
+class Display {
+  constructor(screen) {
+    this.screen = screen;
+    this.clearScreen();
+  }
+
+  get getline1() {
+    return this.line1value;
+  }
+
+  get getline2() {
+    return this.line2value;
+  }
+
+  set setline1(par) {
+    this.line1 = par;
+  }
+
+  set setline2(par) {
+    this.line2 = par;
+  }
+
+  updateScreen() {
+    this.screen[0].textContent = this.line1;
+    this.screen[1].textContent = this.line2;
+  }
+
+  addCharacter(symbol) {
+    this.line1 += symbol;
+    this.updateScreen();
+  }
+
+  removeCharacter() {
+    this.line1 = this.line1.slice(0, -1);
+    this.updateScreen();
+  }
+
+  clearScreen() {
+    this.line1 = '';
+    this.line2 = 0;
+    this.updateScreen();
+  }
+}
+
+class Calculator {
+  constructor() {
+    const screen = [
+      document.querySelector('#displayline1'),
+      document.querySelector('#displayline2')
+    ];
+
+    this.display = new Display(screen);
+
+    const but0 = document.querySelector('#but0');
+    const but1 = document.querySelector('#but1');
+    const but2 = document.querySelector('#but2');
+    const but3 = document.querySelector('#but3');
+    const but4 = document.querySelector('#but4');
+    const but5 = document.querySelector('#but5');
+    const but6 = document.querySelector('#but6');
+    const but7 = document.querySelector('#but7');
+    const but8 = document.querySelector('#but8');
+    const but9 = document.querySelector('#but9');
+    const butTimes = document.querySelector('#butTimes');
+    const butDiv = document.querySelector('#butDiv');
+    const butPlus = document.querySelector('#butPlus');
+    const butMinus = document.querySelector('#butMinus');
+    const butDot = document.querySelector('#butDot');
+    const butC = document.querySelector('#butC');
+    const butEqual = document.querySelector('#butEqual');
+    const butDel = document.querySelector('#butDEL');
+
+    this.addButton(but0, '0');
+    this.addButton(but1, '1');
+    this.addButton(but2, '2');
+    this.addButton(but3, '3');
+    this.addButton(but4, '4');
+    this.addButton(but5, '5');
+    this.addButton(but6, '6');
+    this.addButton(but7, '7');
+    this.addButton(but8, '8');
+    this.addButton(but9, '9');
+    this.addButton(butTimes, 'x');
+    this.addButton(butDiv, '/');
+    this.addButton(butPlus, '+');
+    this.addButton(butMinus, '-');
+    this.addButton(butDot, '.');
+    this.addClearButton(butC);
+    this.addEqualButton(butEqual);
+    this.addDelButton(butDel);
+  }
+
+  addButton(button, symbol) {
     button.addEventListener('click', () => {
-      this.addCharacter(symbol);
+      this.display.addCharacter(symbol);
     });
-  },
-  addClearButton: function (button) {
+  }
+
+  addClearButton(button) {
     button.addEventListener('click', () => {
-      this.clearDisplay();
+      this.display.clearScreen();
     });
-  },
-  addEqualButton: function (button) {
+  }
+
+  addEqualButton(button) {
     button.addEventListener('click', () => {
       this.solveMath();
     });
-  },
-  addDelButton: function (button) {
+  }
+
+  addDelButton(button) {
     button.addEventListener('click', () => {
-      this.removeCharacter();
+      this.display.removeCharacter();
     });
-  },
-  allowedInDisplay: function (symbol) {
+  }
+
+  allowedInDisplay(symbol) {
+    //TODO
     return true;
-  },
-  solveMath: function () {
+  }
+
+  solveMath() {
     //TODO: Make the solver
   }
-};
+}
 
-const but0 = document.querySelector('#but0');
-const but1 = document.querySelector('#but1');
-const but2 = document.querySelector('#but2');
-const but3 = document.querySelector('#but3');
-const but4 = document.querySelector('#but4');
-const but5 = document.querySelector('#but5');
-const but6 = document.querySelector('#but6');
-const but7 = document.querySelector('#but7');
-const but8 = document.querySelector('#but8');
-const but9 = document.querySelector('#but9');
-const butTimes = document.querySelector('#butTimes');
-const butDiv = document.querySelector('#butDiv');
-const butPlus = document.querySelector('#butPlus');
-const butMinus = document.querySelector('#butMinus');
-const butDot = document.querySelector('#butDot');
-const butC = document.querySelector('#butC');
-const butEqual = document.querySelector('#butEqual');
-const butDel = document.querySelector('#butDEL');
-
-display.addBehaviourToButton(but0, '0');
-display.addBehaviourToButton(but1, '1');
-display.addBehaviourToButton(but2, '2');
-display.addBehaviourToButton(but3, '3');
-display.addBehaviourToButton(but4, '4');
-display.addBehaviourToButton(but5, '5');
-display.addBehaviourToButton(but6, '6');
-display.addBehaviourToButton(but7, '7');
-display.addBehaviourToButton(but8, '8');
-display.addBehaviourToButton(but9, '9');
-display.addBehaviourToButton(butTimes, 'x');
-display.addBehaviourToButton(butDiv, '/');
-display.addBehaviourToButton(butPlus, '+');
-display.addBehaviourToButton(butMinus, '-');
-display.addBehaviourToButton(butDot, '.');
-display.addClearButton(butC);
-display.addEqualButton(butEqual);
-display.addDelButton(butDel);
+let calc = new Calculator();
